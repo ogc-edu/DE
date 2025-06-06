f46289d2a080dc320c162ed611d377985208daac
OoiGuanCheng <ooiguancheng1118@1utar.my>
2025-06-07 00:23:02 +0800

    added empty N point crossover function

---
+++ c/nPointCross.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "nPointCross.h"
+
+void nPointCrossover()
+{
+  /*
+  1. N, number of segments are selected
+  2. N divides parent vector into N+1 vectors
+  3. First part taken from Target Vector, then Mutant Vector, alternating
+  */
+}
---
+++ c/nPointCross.h
@@ -0,0 +1,1 @@
+void nPointCrossover();
