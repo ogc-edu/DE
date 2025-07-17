ada82ae8da7672943ca551e30a0587d095863175
OoiGuanCheng <ooiguancheng1118@1utar.my>
2025-07-17 11:14:54 +0800

    STS working with SS = 2, does not work with other SS value

--- s/check.cpp
+++ s/check.cpp
@@ -3,6 +3,9 @@
 #include "../random.h"
 #include "../fitnessEvaluation.h"
 #include "../s/STS40.h"
+#include "../m/best3.h"
+#include "../c/binomialCross.h"
+#include "../m/unique.h"
 
 #include <vector>
 #include <math.h>
@@ -15,56 +18,52 @@ int main()
 {
   double positionVector[10][30];
   double trialVector[10][30];
+  double mutantVector[10][30];
   double rangeMin = -65.536, rangeMax = 65.536;
   int bestPos;
+  int bench = 2;
   init(positionVector, rangeMin, rangeMax, 10, &bestPos, 2); // initialize position vectors once
 
-  for (int i = 0; i < 10; i++)
-  {
-    for (int j = 0; j < 30; j++) // Fixed: j < 30; j++
-    {
-      trialVector[i][j] = 10000;
-    }
-  }
+  // for (int i = 0; i < 10; i++)
+  // {
+  //   for (int j = 0; j < 30; j++) // Fixed: j < 30; j++
+  //   {
+  //     trialVector[i][j] = 10000;
+  //   }
+  // }
 
-  for (int i = 0; i < 30; i++)
-  {
-    trialVector[9][i] = 0;
-  }
+  // for (int i = 0; i < 30; i++)
+  // {
+  //   trialVector[9][i] = 0;
+  // }
+
+  // for (int j = 0; j < 10; j++)
+  // {
+  //   std::cout << "trial vector " << j + 1 << ": ";
+  //   for (int i = 0; i < 30; i++)
+  //   {
+  //     std::cout << trialVector[j][i] << " ";
+  //   }
+  //   std::cout << std::endl;
+  // }
+  Best3Mutation(positionVector, mutantVector, 10, 0.80, rangeMin, rangeMax, 2, bestPos);
+  binomialCrossover(positionVector, mutantVector, trialVector, 0.30, 10);
   for (int j = 0; j < 10; j++)
   {
     std::cout << "target vector " << j + 1 << ": ";
-    for (int i = 0; i < 30; i++)
-    {
-      std::cout << positionVector[j][i] << " ";
-    }
-    std::cout << std::endl;
+    std::cout << fitness(positionVector[j], bench);
+    std::cout << "\ttrial vector " << j + 1 << ": ";
+    std::cout << fitness(trialVector[j], bench) << std::endl;
   }
-  std::cout << "trial" << std::endl;
-  for (int j = 0; j < 10; j++)
-  {
-    std::cout << "trial vector " << j + 1 << ": ";
-    for (int i = 0; i < 30; i++)
-    {
-      std::cout << trialVector[j][i] << " ";
-    }
-    std::cout << std::endl;
-  }
-
   sts(positionVector, trialVector, 2, 10);
 
-  std::cout << "After STS" << std::endl;
-
+  std::cout << "After selection: " << std::endl;
   for (int j = 0; j < 10; j++)
   {
-    std::cout << "target vector " << j + 1 << ": ";
-    for (int i = 0; i < 30; i++)
-    {
-      std::cout << positionVector[j][i] << " ";
-    }
-    std::cout << std::endl;
+    std::cout << "\ntarget vector " << j + 1 << ": ";
+    std::cout << fitness(positionVector[j], bench) << std::endl;
   }
   return 0;
 }
 
-// g++ ../random.cpp ../init.cpp ../fitnessEvaluation.cpp ./STS40.cpp ./check.cpp -o check.exe
+// g++ ../random.cpp ../init.cpp ../fitnessEvaluation.cpp ./STS40.cpp ./check.cpp ../m/best3.cpp ../c/binomialCross.cpp ../m/unique.cpp -o check.exe
